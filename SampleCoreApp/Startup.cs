4923ee5b94e3f3384f2a675914073efd7bc530f4
ashikhpm <ashikhpm313@gmail.com>
2020-08-15 13:45:04 +0530

    obsolete added

--- SampleCoreApp/Startup.cs
+++ SampleCoreApp/Startup.cs
@@ -25,6 +25,7 @@ namespace SampleCoreApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
